836277687523fa6a4119d997cfb8a44fa9b38243
Thiago Stteffen <thiagokalu@gmail.com>
2021-06-17 20:15:51 -0300

    adicionado o exercicio do array

---
+++ src/Exemplo_array.java
@@ -0,0 +1,16 @@
+
+public class Exemplo_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int teste [] = new int [5];
+		Random gerador = new Random();
+		
+	}
+	for (int i =0;
+			i<100;
+			i++) 
+	{}
+		
+
+}
--- src/Vetores.java
+++ src/Vetores.java
@@ -4,48 +4,19 @@ public class Vetores {
 
 	public static void main(String[] args) {
 		
-		double aluno1, aluno2, aluno3, aluno4, aluno5, aluno6, aluno7, aluno8, aluno9, aluno10, total;
-		Scanner leitor = new Scanner(System.in);
-		
-		System.out.println("Por favor, digite o consumo do 1º aluno");
-		aluno1 = leitor.nextDouble();
-		System.out.println("Por favor, digite o consumo do 2º aluno");
-		aluno2 = leitor.nextDouble();
-		System.out.println("Por favor, digite o consumo do 3º aluno");
-		aluno3 = leitor.nextDouble();
-		System.out.println("Por favor, digite o consumo do 4º aluno");
-		aluno4 = leitor.nextDouble();
-		System.out.println("Por favor, digite o consumo do 5º aluno");
-		aluno5 = leitor.nextDouble();
-		System.out.println("Por favor, digite o consumo do 6º aluno");
-		aluno6 = leitor.nextDouble();
-		System.out.println("Por favor, digite o consumo do 7º aluno");
-		aluno7 = leitor.nextDouble();
-		System.out.println("Por favor, digite o consumo do 8º aluno");
-		aluno8 = leitor.nextDouble();
-		System.out.println("Por favor, digite o consumo do 9º aluno");
-		aluno9 = leitor.nextDouble();
-		System.out.println("Por favor, digite o consumo do 10º aluno");
-		aluno10 = leitor.nextDouble();
-		
-		total = aluno1 + aluno2 + aluno3 + aluno4 + aluno5 + aluno6 + aluno7 + aluno8 + aluno9 + aluno10;
-		
-		System.out.println("O aluno 1 consumiu R$" + aluno1);
-		System.out.println("O aluno 2 consumiu R$" + aluno2);
-		System.out.println("O aluno 3 consumiu R$" + aluno3);
-		System.out.println("O aluno 4 consumiu R$" + aluno4);
-		System.out.println("O aluno 5 consumiu R$" + aluno5);
-		System.out.println("O aluno 6 consumiu R$" + aluno6);
-		System.out.println("O aluno 7 consumiu R$" + aluno7);
-		System.out.println("O aluno 8 consumiu R$" + aluno8);
-		System.out.println("O aluno 9 consumiu R$" + aluno9);
-		System.out.println("O aluno 10 consumiu R$" + aluno10);
-
-		System.out.println("O total de consumo foi de R$" + total);
-		
-		leitor.close();
-		
-
-	}
-
+		//Para criar um vetor, tipo nome[]= new tipo[tamanho];
+        double alunos[] = new double[10];
+        double total=0;
+        Scanner leitor = new Scanner(System.in);
+        for(int i=0; i<10;i++) {
+            System.out.println("Digite o consumo do " + (i+1) + "º aluno");
+            alunos[i] = leitor.nextDouble();
+            total = total + alunos[i];
+        }
+        for(int i=0;i<10;i++) {
+            System.out.println("O aluno " + (i+1) + " consumiu R$" + alunos[i]);
+        }
+        System.out.println("O total de consumo foi de R$" + total);
+        leitor.close();
+}
 }
